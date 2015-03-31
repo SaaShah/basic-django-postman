3b34385b001fd2e5a4a424dcca9cdd2ea2795f70
Saad Bin Shahid <saadbinshahid.fb@gmail.com>
2015-03-31 15:38:27 +0500

    Basic django-master implemented.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # basic-django-postman
 A sample project using basic django-postman with my personal-django-base.
+
+# django-postman
+doc: http://django-postman.readthedocs.org/en/latest/
+
+# Err101 - Connection Refused
+python -m smtpd -n -c DebuggingServer localhost:1025
+Use this for a development debugging server.
--- main/settings.py
+++ main/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
     'bootstrap3',
     'twitter_bootstrap',
     'landing',
+    'postman',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -155,3 +156,28 @@ PIPELINE_JS = {
 PIPELINE_COMPILERS = (
     'pipeline.compilers.less.LessCompiler',
 )
+
+
+
+# postman app settings
+
+# POSTMAN_DISALLOW_ANONYMOUS = True  # default is False
+# POSTMAN_DISALLOW_MULTIRECIPIENTS = True  # default is False
+# POSTMAN_DISALLOW_COPIES_ON_REPLY = True  # default is False
+# POSTMAN_DISABLE_USER_EMAILING = True  # default is False
+POSTMAN_AUTO_MODERATE_AS = True  # default is None
+# POSTMAN_SHOW_USER_AS = 'get_full_name'  # default is None
+# POSTMAN_QUICKREPLY_QUOTE_BODY = True  # default is False
+# POSTMAN_NOTIFIER_APP = None  # default is 'notification'
+# POSTMAN_MAILER_APP = None  # default is 'mailer'
+# POSTMAN_AUTOCOMPLETER_APP = {
+    # 'name': '',  # default is 'ajax_select'
+    # 'field': '',  # default is 'AutoCompleteField'
+    # 'arg_name': '',  # default is 'channel'
+    # 'arg_default': 'postman_friends',  # no default, mandatory to enable the feature
+# }  # default is {}
+
+
+# Development Email Settings, to be changed
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
